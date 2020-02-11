c45e06088752725a783189d4d35f538e45905adb
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2020-02-11 16:42:08 +0000

    updatedomain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "authentication-app-rails-isjr.heroku.com"
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "authentication-app-rails-isjr.herokuapp.com"
 else
  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end

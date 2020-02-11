53efb1f009fd71d7dc454bd4c7aed43ba1b7c4ad
Francisco Gaivao <francisco.abecassis.gaivao@gmail.com>
2020-02-11 18:11:06 +0000

    .

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ def create
   user = User.create(
     username: params['user']['username'],
     password: params['user']['password'],
-    password_confirmation: params['password_confirmation']['password_confirmation']
+    password_confirmation: params['user']['password_confirmation']
     )
 
   if user
